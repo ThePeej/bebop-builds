23c6c761feb6d8e39a5d3e2befad789661c25603
PJ Frias <PeejFrias@Gmail.com>
2018-05-02 17:12:51 -0400

    add fonts to Landing page and add Sidebar component to potentially use in lieu of navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Sidebar from './components/Sidebar'
 import Landing from './components/Landing'
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p> */}
+        {/* <Sidebar/> */}
         <Landing/>
       </div>
     );
---
+++ src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Sidebar = () => (
+  <div id="sidebar">
+    <h1>SIDEBAR</h1>
+  </div>
+);
+
+export default Sidebar;
