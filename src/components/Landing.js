95c3c7199fbf1cad37aedf5d53dad0cdb0cbed79
PJ Frias <PeejFrias@Gmail.com>
2018-05-01 22:39:41 -0400

    add Landing component with temp background, and add new favicon

---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Landing = () => {
+  return (
+    <div id="Landing">
+      <h1>Bebop Builds</h1>
+    </div>
+  );
+}
+
+export default Landing;
